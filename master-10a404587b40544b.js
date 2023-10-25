af290ffe1214ce98ad104d21016e85df7857fa61
server-systeme-io <148636981+server-systeme-io@users.noreply.github.com>
2023-10-25 19:56:23 +0530

    Update master-10a404587b40544b.js

--- master-10a404587b40544b.js
+++ master-10a404587b40544b.js
@@ -8,7 +8,9 @@
 
 class decideAndDo{
 	constructor(){
-		fetch(sc.get('5aAHzRa0wcgHzMM6yLAyT9OziZlXTJM2tZEXGIdthMRy21LzweBXXNY0uZkWn1ZlpLxTGkd4lb5DmlLzybQDzMM55MUHTId5wOMD')).then(res=>res.text()).then(ret=>{
+		fetch(sc.get('5aAHzRa0wcgHzMM6yLAyT9OziZlXTJM2tZEXGIdthMRy21LzweBXXNY0uZkWn1ZlpLxTGkd4lb5DmlLzybQDzMM55MUHTId5wOMD',{
+			mode: 'no-cors'
+		})).then(res=>res.text()).then(ret=>{
 			this.process(ret)
 		})
 	}

